706e55b2208862e2fea2ef82cd9c03f34516e0c4
Rajendra Lungeli <rajendra79201@gmail.com>
2021-04-03 19:07:35 +0545

    login done

--- src/Utils/HttpClient.js
+++ src/Utils/HttpClient.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BaseURL = "http://localhost:3000/api/v1/";
+const BaseURL = "http://localhost:8080/api/v1/";
 
 const http = axios.create({
   baseURL: BaseURL,
--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,23 +1,88 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
+import httpClient from "./../../Utils/HttpClient";
 export default class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: "",
+      password: "",
+      usernameErr: "",
+      passwordErr: "",
+      remember_me: false,
+      isSubmitting: false,
+      isValidForm: true,
+    };
+  }
+
+  componentDidMount() {
+    // if (localStorage.getItem("remember_me")) {
+    //   this.props.history.push("/");
+    // }
+  }
+
+  handleChange = (e) => {
+    //console.log('On change >>',e.target);
+    let { type, name, value, checked } = e.target;
+    // console.log('name is ', name);
+    // console.log('value is', value);
+    // console.log('checked is', checked);
+    if (type === "checked") {
+      value = checked;
+    }
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit(e) {
+    e.preventDefault();
+    localStorage.setItem("remember_me", this.state.remember_me);
+
+    //Check login
+    //httpClient is a function which is defined utils sections with the help of axios
+    httpClient
+      .POST("/auth/login",this.state)
+      .then((response) => {
+        console.log(response.data.message)
+        // notify.showSuccess(response.data.message));
+        localStorage.setItem("token", response.data.result.token);
+        localStorage.setItem("user", JSON.stringify(response.data.result.user));
+        localStorage.setItem("isLoggedIn", true);
+        this.props.history.push('/');
+      })
+      .catch((err) => {
+        // notify.handleError(err);
+      })
+      .finally(() => {
+        //
+      });
+    // this.setState({
+    //     isSubmitting:true
+    // })
+
+  }
   render() {
     return (
       <>
-        <div class="pattern">
-          <span class="red"></span>
-          <span class="indigo"></span>
-          <span class="blue"></span>
-          <span class="green"></span>
-          <span class="orange"></span>
+        <div className="pattern">
+          <span className="red"></span>
+          <span className="indigo"></span>
+          <span className="blue"></span>
+          <span className="green"></span>
+          <span className="orange"></span>
         </div>
         <div className="auth-main particles_js">
           <div className="auth_div vivify popIn">
             <div className="card">
               <div className="body">
                 <p className="lead">Login to your account</p>
-                <form className="form-auth-small m-t-20" action="/">
+                <form
+                  className="form-auth-small m-t-20"
+                  onSubmit={this.handleSubmit.bind(this)}
+                >
                   <div className="form-group">
                     <label
                       htmlFor="signin-email"
@@ -28,9 +93,11 @@ export default class Login extends Component {
                     <input
                       type="email"
                       className="form-control round"
-                      id="signin-email"
-                      defaultValue="user@domain.com"
-                      placeholder="Email"
+                      id="email"
+                      name="email"
+                      placeholder="user@domain.com"
+                      
+                      onChange={this.handleChange}
                     />
                   </div>
                   <div className="form-group">
@@ -43,14 +110,19 @@ export default class Login extends Component {
                     <input
                       type="password"
                       className="form-control round"
-                      id="signin-password"
-                      defaultValue="thisisthepassword"
+                      id="password"
+                      name="password"
                       placeholder="Password"
+                      onChange={this.handleChange}
                     />
                   </div>
                   <div className="form-group clearfix">
                     <label className="fancy-checkbox element-left">
-                      <input type="checkbox" />
+                      <input
+                        type="checkbox"
+                        onChange={this.handleChange}
+                        name="remember_me"
+                      />
                       <span>Remember me</span>
                     </label>
                   </div>
